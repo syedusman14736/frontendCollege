0a83f4dda5273d7c68828d552d50428f8a01ee26
Muhammad Usman <aifisolutionshub@gmail.com>
2025-10-25 21:40:55 +0500

    trying fixing styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ import Gallery from "./pages/Gallery/Gallery";
 import About from "./pages/about/About";
 import News from "./pages/News/News";
 import Policies from './pages/Policies/Policies'
+import 'remixicon/fonts/remixicon.css';
+import './styles/bootstrap.min.css';
+import './styles/fontawesome.min.css'
+import './styles/animate.min.css';
+import './styles/venobox.min.css';
+import './styles/keyframe-animation.css';
+import './styles/odometer.min.css';
+import './styles/nice-select.css';
+import './styles/daterangepicker.css';
+import './styles/swiper.min.css';
+import './styles/main.css';
+import './styles/index.css';
 
 function App() {
   return (
